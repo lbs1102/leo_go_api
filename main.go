ced4851ef685155ac772b8f4fa45560a654db25a
lbs1102 <tff9276z@gmail.com>
2022-06-08 14:58:16 +0800

    test

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
-import (
-	"fmt"
-
-	"leo_go_api/api"
-	"leo_go_api/api/common"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-)
+import "fmt"
 
 func main() {
-	app := fiber.New()
-	app.Use(cors.New(
-	/*cors.Config{
-		Next: nil,
-		// AllowOrigins:     "http://127.0.0.1",
-		AllowOrigins:     "",
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
-		AllowHeaders:     "",
-		AllowCredentials: false,
-		ExposeHeaders:    "",
-		MaxAge:           0,
-	}*/))
-	api.InitRouter(app)
-	app.Listen(fmt.Sprintf(":%s", common.Port))
+	fmt.Println("Test yml")
+	// app := fiber.New()
+	// app.Use(cors.New(
+	// /*cors.Config{
+	// 	Next: nil,
+	// 	// AllowOrigins:     "http://127.0.0.1",
+	// 	AllowOrigins:     "",
+	// 	AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+	// 	AllowHeaders:     "",
+	// 	AllowCredentials: false,
+	// 	ExposeHeaders:    "",
+	// 	MaxAge:           0,
+	// }*/))
+	// app.Get("/test", func(c *fiber.Ctx) error {
+	// 	// Get session from storage
+	// 	sess, err := store.Get(c)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	fmt.Println(sess)
+	// 	// Get all Keys
+	// 	keys := sess.Keys()
+	// 	fmt.Println(keys)
+	// 	// Get value
+	// 	name := sess.Get("name")
+	// 	fmt.Println(name)
+	// 	// Set key/value
+	// 	sess.Set("name", "john")
+	// 	// Delete key
+	// 	// sess.Delete("name")
+	// 	// // Destroy session
+	// 	// if err := sess.Destroy(); err != nil {
+	// 	// 	panic(err)
+	// 	// }
+	// 	// Save session
+	// 	if err := sess.Save(); err != nil {
+	// 		panic(err)
+	// 	}
+	// 	return c.SendString(fmt.Sprintf("Welcome %v", name))
+	// })
+	// api.InitRouter(app)
+	// app.Listen(fmt.Sprintf(":%s", common.Port))
 }
