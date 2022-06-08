d486bd0b7becd2b7e59021d54565c0e44c9526eb
lbs1102 <tff9276z@gmail.com>
2022-06-08 15:07:38 +0800

    test

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"leo_go_api/api"
+	"leo_go_api/api/common"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
-	fmt.Println("Test yml")
-	// app := fiber.New()
-	// app.Use(cors.New(
-	// /*cors.Config{
-	// 	Next: nil,
-	// 	// AllowOrigins:     "http://127.0.0.1",
-	// 	AllowOrigins:     "",
-	// 	AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
-	// 	AllowHeaders:     "",
-	// 	AllowCredentials: false,
-	// 	ExposeHeaders:    "",
-	// 	MaxAge:           0,
-	// }*/))
+	app := fiber.New()
+	// store := session.New()
+	app.Use(cors.New(
+	/*cors.Config{
+		Next: nil,
+		// AllowOrigins:     "http://127.0.0.1",
+		AllowOrigins:     "",
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH",
+		AllowHeaders:     "",
+		AllowCredentials: false,
+		ExposeHeaders:    "",
+		MaxAge:           0,
+	}*/))
 	// app.Get("/test", func(c *fiber.Ctx) error {
 	// 	// Get session from storage
 	// 	sess, err := store.Get(c)
@@ -43,6 +51,6 @@ func main() {
 	// 	}
 	// 	return c.SendString(fmt.Sprintf("Welcome %v", name))
 	// })
-	// api.InitRouter(app)
-	// app.Listen(fmt.Sprintf(":%s", common.Port))
+	api.InitRouter(app)
+	app.Listen(fmt.Sprintf(":%s", common.Port))
 }
